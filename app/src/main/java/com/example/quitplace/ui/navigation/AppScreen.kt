cad8ed02c51804518239697a9452a7561efe5ac0
azimzanryskulov <agent220101@gmail.com>
2025-11-18 23:12:49 +0100

    Remove AppScreen navigation class as part of project refactoring

--- app/src/main/java/com/example/quitplace/ui/navigation/AppScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quitplace.ui.navigation
-
-sealed class AppScreen(val route: String) {
-    object Feed : AppScreen("feed")
-    object CreatePost : AppScreen("create_post")
-    data class PostDetails(val postId: String) : AppScreen("post_details/{postId}") {
-        fun createRoute(postId: String) = "post_details/$postId"
-    }
-}
